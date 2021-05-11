5d562292f8adb89307bbc44ab4c869e139797264
Catdog5452 <69901798+Catdog5452@users.noreply.github.com>
2021-05-11 13:26:56 +1200

    database connected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  state = {
+    loading: true,
+    person: null,
+  }
+
+  async componentDidMount(){
+    const url = "https://scpdatabase-0fe9.restdb.io/rest/data";
+    const response = await fetch(url, {method: 'GET',
+      headers: 
+      { 'cache-control': 'no-cache',
+          'x-apikey': '6099da8fe3b6e02545eda6a8' }});
+    const data = await response.json();
+    this.setState({person: data[0], loading: false});
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.loading || !this.state.person ? (
+          <div>loading...</div>
+        ) : (
+          <div>
+            <div>{this.state.person.scpNumber}</div>
+            <div>{this.state.person.scpObjectClass}</div>
+            <div>{this.state.person.scpSCP}</div>
+            <div>{this.state.person.scpDescription}</div>
+          </div>
+        )}
+      </div>
+    );
+  }
+
 }
-
-export default App;
